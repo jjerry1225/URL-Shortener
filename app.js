591bb3355aa892f08e5d5955471db55c9644d3b0
jjerry1225 <i183801225@gmail.com>
2022-12-28 16:43:22 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const PORT = 3000;
 
-const URL =require('./models/url')
+const URL = require("./models/url");
 
 const app = express();
 
 // 引用連線mongoose的檔案，對 app.js 而言，Mongoose 連線設定只需要「被執行」，不需要接到任何回傳參數繼續利用，所以這裡不需要再設定變數。
 require("./config/mongoose");
 
+// 套用express-handlebars的模板引擎
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // setting route
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // start and listen server
 app.listen(PORT, () => {
